b708922d2227c7e7555d25d75cff920bbd1cc332
fguardiola <fernando.guardiola.888@gmail.com>
2020-04-30 19:21:12 +0200

    331. Render resource list

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -25,7 +25,7 @@ const ResourceList = ({ resource }) => {
     }, [resource])
 
     return (
-        <div> {resources.length} </div>
+        <ul> {resources.map(record => <li key={record.id}>{record.title} </li>)} </ul>
     );
 
 }
