cc7debd0577980dc9ad2cb0574fc5ec05426ebbd
fguardiola <fernando.guardiola.888@gmail.com>
2020-04-30 18:36:55 +0200

    325ComponentDidUpdate vs

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -6,10 +6,20 @@ class ResourceList extends React.Component {
     state = {
         resources: []
     }
+
     async componentDidMount() {
         const respose = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
 
         this.setState({ resources: respose.data })
+    }
+    // componentDidUpdate is called every time the parent rerender or setState is called 
+    async componentDidUpdate(prevProps) {
+        //only make rquest when resources prop change 
+        if (prevProps !== this.props) {
+            const respose = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+
+            this.setState({ resources: respose.data })
+        }
     }
     render() {
         return (
